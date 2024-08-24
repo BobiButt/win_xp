b231170b092793cf28e1f12c2b5b8183c69c465d
BOBI <mubeenbutt853@gmail.com>
2024-08-24 12:33:22 +0500

    upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const start = ()=>{
+    const st = document.getElementById('start_menu');
+    st.classList.toggle('show');
+}
+const search = ()=>{
+    
+    const search = document.getElementById('search');
+    search.classList.toggle('open');
+}
+const power = ()=>{
+    
+    const shutt = document.getElementById('shuttdown');
+    shutt.classList.toggle('open');
+}
+const shutt_cancel = ()=>{
+    
+    const shutt_d = document.getElementById('shuttdown');
+    shutt_d.classList.toggle('open');
+}
+
+function alrt() {
+    alert("ahmm ahmm its not easy. it will take some time to complete")
+}
+function shutt_alrt() {
+    alert("try maybe work for u hahahaahaha")
+}
