c889b6b32c102445ce6f6d2af55ad0608b67f21e
BobiButt <118150902+BobiButt@users.noreply.github.com>
2024-08-31 04:34:50 +0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+// const off = ()=>{
+   
+//     const st = document.getElementById('start_menu');
+//     const search = document.getElementById('search');
+//     const shutt = document.getElementById('shuttdown');
+//     const all_p_b = document.getElementById('all_program');
+ 
+//     st.style.display='none';
+//     search.style.display='none';
+//     shutt.style.display='none';
+//     all_p_b.style.display='none';
+
+    
+
+
+// }
 const start = ()=>{
     const st = document.getElementById('start_menu');
     st.classList.toggle('show');
@@ -23,4 +39,13 @@ function alrt() {
 }
 function shutt_alrt() {
     alert("try maybe work for u hahahaahaha")
+}
+// all programs
+// const all_p_block = ()=>{
+//     const ap_b = document.getElementById('all_program');
+//     ap_b.classList.('p_display_block');
+// }
+const all_p = ()=> {
+    const all_p_b = document.getElementById('all_program');
+all_p_b.classList.toggle('p_display_block')
 }
